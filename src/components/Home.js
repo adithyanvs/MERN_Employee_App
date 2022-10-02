3ae19379643e0773e1b2d1023a4d56c0ad3183aa
adithyanvs <adithyanvs@gmail.com>
2022-10-02 11:38:14 +0530

    first

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='mt-5'>
+    <div className='add_btn mt-2'>
+        <button className='btn btn-primary'>Add Data</button>
+    </div>
+    </div>
+  )
+}
+
+export default Home
